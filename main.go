2490ea6bfc05b6c0e1b3bd497d9130bf9b84d80a
bokyun91 <braun@todayhenry.co>
2020-12-18 21:33:07 +0900

    Dictionary part One

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bokyun91/dict_nomad/mydict"
+)
+
+func main() {
+	dictionary := mydict.Dictionary{"first": "First word"}
+	definition, err := dictionary.Search("first")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
+	}
+}
